39d8adf55874b0a0c22f42b5bfcc9c053157e63f
Oyeyemi <mac@Oyeyemi-Korede.local>
2025-12-09 12:39:06 +0100

    update

--- components/comingSoon/index.tsx
+++ components/comingSoon/index.tsx
@@ -10,7 +10,7 @@ const ComingSoon = () => {
           <button>COMING SOON</button>
           <h2>
             Explore your city or the world with<span> stories, games, </span>and
-            <span>quests</span> that make every journey unforgettable.
+            <span> quests</span> that make every journey unforgettable.
           </h2>
           <Link href="#">Join Waitlist</Link>
         </div>
