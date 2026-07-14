d74805387597b2b53d4ef613f0df0a5b71e24a2b
Oyeyemi Korede <oyeyemikorede5@gmail.com>
2026-07-14 11:27:53 +0100

    Update

--- utils/magazineData.ts
+++ utils/magazineData.ts
@@ -9,13 +9,20 @@ export interface Magazine {
 export const magazineData: Magazine[] = [
     {
         id: 1,
+        title: "Geotela Magazine Vol. 3",
+        description: "Your Money Playbook for the World Cup",
+        image: "/assets/magazine-3.jpg",
+        pdfUrl: "/magazines/Geotela-Magazine-Volume-3.pdf",
+    },
+    {
+        id: 2,
         title: "Geotela Magazine Vol. 2",
         description: "Your Money Playbook for the World Cup",
         image: "/assets/volume2.jpg",
         pdfUrl: "/magazines/Geotela-Magazine-Volume-2.pdf",
     },
     {
-        id: 2,
+        id: 3,
         title: "Geotela Magazine Vol. 1",
         description: "See the World Differently",
         image: "/assets/volume1.jpeg",
